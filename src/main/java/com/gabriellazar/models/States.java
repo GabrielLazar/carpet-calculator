f26e3e7612579b4f4bb8fbb3ce59a58ca414a7ac
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2020-10-28 13:06:03 -0500

    Add states

--- src/main/java/com/gabriellazar/models/States.java
+++ src/main/java/com/gabriellazar/models/States.java
@@ -5,13 +5,30 @@ public enum States {
     VA("Arlington","Fairfax","McLean"),
     TX("Dallas","Austin","San Antonio");
 
-    private  String [] cities;
+    private  List<String> cities;
     static final private Map<String,States> mapOfCities = new HashMap<>();
 
     States(String ... cities) {
-       this.cities = cities;
+       this.cities = Arrays.asList(cities);
     }
 
+    static{
+        for(States state: States.values()){
+            for(String city: state.cities){
+                mapOfCities.put(city,state);
+            }
+
+        }
+    }
+
+    public static boolean hasCity(String value){
+
+        return mapOfCities.containsKey(value);
+    }
+
+    public static States getState(String city){
+       return mapOfCities.get(city);
+    }
 
 
 }
